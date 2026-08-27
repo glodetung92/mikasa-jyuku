93d1528b8461872beb42b44a0cdda9b261227ea0
glodetung92 <glodetung92@gmail.com>
2026-08-27 12:53:39 +0900

    Apply automatic domain detection

--- controller/db_connection.php
+++ controller/db_connection.php
@@ -1,14 +1,28 @@
 <?php
-function OpenCon() {
-    $dbhost = "mysql633.db.sakura.ne.jp";
-    $dbuser = "kogaku-sha";
-    $dbpass = "L-pt18HL";
-    $db = "kogaku-sha_mikasa_hp";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+function OpenCon()
+{
+    $is_local = (isset($_SERVER['HTTP_HOST']) && in_array($_SERVER['HTTP_HOST'], ['localhost', 'mikasajyuku.test'])) 
+                || (isset($_SERVER['REMOTE_ADDR']) && in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']))
+                || (php_sapi_name() === 'cli');
+
+    if ($is_local) {
+        $dbhost = "127.0.0.1";
+        $dbuser = "root";
+        $dbpass = "";
+        $db = "kogaku-sha_mikasa_hp";
+    } else {
+        $dbhost = "mysql633.db.sakura.ne.jp";
+        $dbuser = "kogaku-sha";
+        $dbpass = "L-pt18HL";
+        $db = "kogaku-sha_mikasa_hp";
+    }
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
     return $conn;
 }
- 
-function CloseCon($conn) {
-    $conn -> close();
+
+function CloseCon($conn)
+{
+    $conn->close();
 }
